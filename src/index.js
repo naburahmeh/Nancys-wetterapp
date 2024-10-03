e00bdc4423a74eb8b2cc39593d4effbe725f0429
naburahmeh <aburahmehnncy@gmail.com>
2024-10-04 00:03:35 +0200

    added two funtions

--- src/index.js
+++ src/index.js
@@ -1,8 +1,20 @@
+function updateTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  temperatureElement.innerHTML = response.data.temperature.current;
+}
+
+function searchCity(city) {
+  let apiKey = "b33862d908c4feta1cbo2fc015504418";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateTemperature);
+}
+
 function handleSearch(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
