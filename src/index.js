a6115b817c7362a32a23694c628c93e4ab44ff79
naburahmeh <aburahmehnncy@gmail.com>
2024-10-05 23:14:31 +0200

    changed date with functions

--- src/index.js
+++ src/index.js
@@ -5,12 +5,17 @@ function updateTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let currentDayElement = document.querySelector("#currentDay");
+  let date = new Date(response.data.time * 1000);
 
+
+  timeElemnt.innerHTML = `${date.getDay()} ${date.getHours()}:${date.getMinutes()};
   temperatureElement.innerHTML = Math.round(temperature);
   descriptionElement.innerHTML = response.data.condition.description;
   cityElement.innerHTML = response.data.city;
   humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
   windElement.innerHTML = `${response.data.wind.speed}km/h`;
+  currentDayElement.innerHTML = response.data.
 }
 
 function searchCity(city) {
@@ -18,6 +23,11 @@ function searchCity(city) {
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(updateTemperature);
 }
+
+function formatDate(date) {
+    
+}
+
 
 function handleSearch(event) {
   event.preventDefault();
